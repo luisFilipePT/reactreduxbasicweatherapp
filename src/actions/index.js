58e73bc9065b8dba908e80e28bce2efd767a5806
Luís <lf@void.pt>
2016-12-16 17:37:20 +0000

    starting on middleware

---
+++ src/actions/index.js
@@ -0,0 +1,1 @@
+const API_KEY = 'b2514307517b25a0ee8cafc35ef8ff05';
---
+++ src/containers/search_bar.js
@@ -0,0 +1,44 @@
+/**
+ * Created by luis on 16/12/2016.
+ */
+import React, { Component } from 'react';
+
+export default class SearchBar extends Component {
+    constructor(props) {
+        super(props);
+
+        this.state = {term: ''};
+
+        this.onInputChange = this.onInputChange.bind(this);
+    }
+
+    onInputChange(event) {
+        console.log(event.target.value);
+
+        this.setState({term: event.target.value});
+    }
+
+    onFormSubmit(event) {
+        event.preventDefault();
+
+        // We need to go and fetch the data
+    }
+
+    render() {
+        return(
+            <form
+                onSubmit={this.onFormSubmit}
+                className="input-group">
+                <input
+                    placeholder="Get a five day forecast in your favourite cities"
+                    className="form-control"
+                    value={this.state.term}
+                    onChange={this.onInputChange}
+                    type="text"/>
+                <span className="input-group-btn">
+                    <button type="submit" className="btn btn-secondary">Submit</button>
+                </span>
+            </form>
+        );
+    }
+}
