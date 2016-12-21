85678acb9c227b14bd06554bd03fcef505fd3316
Luís <lf@void.pt>
2016-12-21 18:02:34 +0000

    breaking

--- src/actions/index.js
+++ src/actions/index.js
@@ -3,7 +3,7 @@ import axios from 'axios';
 const API_KEY = 'b2514307517b25a0ee8cafc35ef8ff05';
 const ROOT_URL = `http://api.openweathermap.org/data/2.5/forecast?appid=${API_KEY}`;
 
-export const FETCH_WEATHER = 'FETCH_WEATHER';
+const FETCH_WEATHER = 'FETCH_WEATHER';
 
 export function fetchWeather(city) {
     const url = `${ROOT_URL}&q=${city},us`;
@@ -14,3 +14,5 @@ export function fetchWeather(city) {
         payload: request
     }
 }
+
+export default FETCH_WEATHER;
